f3f516d3d7a0002a5c5007a140429448b64e1c93
babyjakdev <cacaparter@gmail.com>
2024-09-05 16:15:46 +0200

    Update core.clj

--- src/blueskyack/core.clj
+++ src/blueskyack/core.clj
@@ -1,20 +1,15 @@
-(ns blueskyack.core
-  (:import [java.io File]
-           [java.util Random]))
+(ns blueskyack.core)
 (require '[clojure.java.io :as io]
          '[etaoin.api :as e]
          '[etaoin.keys :as k])
 
 (defn random-img-path []
-  (let [dir (File. "img")
-        files (filter #(.isFile %) (.listFiles dir))
-        count (count files)]
-    (if (zero? count)
-      nil
-      (let [random-index (.nextInt (Random.) count)]
-        (nth files random-index)))))
+  (let [directory (io/file "img")
+        files (filter #(.isFile %) (file-seq directory))
+        random-file (rand-nth files)]
+    (io/file "img" (.getName random-file))))
 
-(defn bsky-login [driver user pass pps]
+(defn bsky-login [driver user pass]
   (let [sign-xpath ".//button[.//div[normalize-space(text())='Sign in']]"]
     (e/wait-visible driver sign-xpath)
     (e/click driver sign-xpath))
@@ -29,23 +24,33 @@
   (let [emailver-xpath ".//div[div[text()='Not right now']]"]
     (e/wait-visible driver emailver-xpath)
     (e/click driver emailver-xpath))
-  (let [newpost-xpath ".//button[contains(@aria-label, 'New post')]"]
-    (e/wait-visible driver newpost-xpath)
-    (e/click driver newpost-xpath))
-  (let [content-xpath ".//div[@contenteditable='true']"]
-    (e/wait-visible driver content-xpath)
-    (e/fill driver content-xpath (slurp "message.txt")))
-  (let [upload-xpath ".//button[@aria-label='Gallery']"]
-    (e/wait-visible driver upload-xpath)
-    (e/upload-file driver upload-xpath (random-img-path) (e/wait 30))))
+  (loop []
+    (let [composer-xpath ".//button[contains(@aria-label, 'New post')]"]
+      (e/wait-visible driver composer-xpath)
+      (e/click driver composer-xpath))
+    (let [content-xpath ".//div[@contenteditable='true']"]
+      (e/wait-visible driver content-xpath)
+      (e/fill driver content-xpath (slurp "message.txt")))
+    (let [upload-xpath ".//button[@aria-label='Gallery']"]
+      (e/wait-visible driver upload-xpath)
+      (e/click driver upload-xpath)
+      (let [input-xpath "//input[@type='file' and @accept='image/*']"]
+        (e/wait-exists driver input-xpath)
+        (e/js-execute driver "document.querySelector('input[type=\"file\"]').style.display = 'block';")
+        (e/upload-file driver input-xpath (random-img-path))))
+    (let [publish-xpath ".//button[@aria-label='Publish post']"]
+      (e/wait-visible driver publish-xpath)
+      (e/click driver publish-xpath))
+    (e/wait pps)
+    (recur)))
 
 (defn -main [& args]
-  (let [driver (e/firefox)
+  (let [driver (e/chrome-headless)
         username (nth args 0)
         password (nth args 1)
         pps      (Integer. (nth args 2))]
   (e/go driver "https://bsky.app")
   (e/set-window-size driver {:width 1280 :height 800})
-  (bsky-login driver username password pps)
+  (bsky-login driver username password)
   (bsky-poster driver pps)
   (e/quit driver)))
